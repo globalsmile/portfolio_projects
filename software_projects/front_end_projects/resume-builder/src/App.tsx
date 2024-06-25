22f9c569a078eae72fe24628391df32b9caf1813
globalsmile <mohammedbadmus823@gmail.com>
2024-06-25 17:25:35 +0100

    add: ResumeForm and ResumePreview

--- software_projects/front_end_projects/resume-builder/src/App.tsx
+++ software_projects/front_end_projects/resume-builder/src/App.tsx
@@ -5,11 +5,17 @@ import './styles/global.scss';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>Resume Builder</h1>
-      <ResumeForm />
-      <ResumePreview />
-    </div>
+    <div className="app-container">
+      <div className="form-container">
+        <h1>Resume Builder</h1>
+        <p className="instructions">Fill out the form to create your resume</p>
+        <ResumeForm />
+        </div>
+      <div className="preview-container">
+        <h1>Resume Preview</h1>
+        <ResumePreview />
+        </div>
+      </div>
   );
 };
 
