fd7c32df5d722bf1bb79a958e5a6dce55f50f9d0
globalsmile <mohammedbadmus823@gmail.com>
2024-07-23 19:48:12 +0100

    fix:resumePreview

--- software_projects/front_end_projects/resume-builder/src/components/ResumePreview.tsx
+++ software_projects/front_end_projects/resume-builder/src/components/ResumePreview.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ResumeState } from '../types';
 import './ResumePreview.scss';
 
 interface Education {
---
+++ software_projects/front_end_projects/resume-builder/src/types.ts
@@ -0,0 +1,41 @@
+// src/types.ts
+export interface Education {
+  institution: string;
+  degree: string;
+  year: string;
+}
+
+export interface Experience {
+  company: string;
+  role: string;
+  duration: string;
+}
+
+export interface ResumeState {
+  name: string;
+  contact: string;
+  education: Education[];
+  experience: Experience[];
+  skills: string[];
+}
+// src/types.ts
+export interface Education {
+  institution: string;
+  degree: string;
+  year: string;
+}
+
+export interface Experience {
+  company: string;
+  role: string;
+  duration: string;
+}
+
+export interface ResumeState {
+  name: string;
+  contact: string;
+  education: Education[];
+  experience: Experience[];
+  skills: string[];
+}
+
