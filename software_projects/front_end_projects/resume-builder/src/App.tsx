328e3b7ea10baad48c0d021b303b1e8bd75b42ac
globalsmile <mohammedbadmus823@gmail.com>
2024-07-23 19:20:07 +0100

    fix: ResumeForm component

--- software_projects/front_end_projects/resume-builder/src/App.tsx
+++ software_projects/front_end_projects/resume-builder/src/App.tsx
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ResumeForm from './components/ResumeForm';
 import ResumePreview from './components/ResumePreview';
-import './styles/App.scss';
+import './App.css';
+
+import { ResumeState } from './types';
 
 const App: React.FC = () => {
+  const [resume, setResume] = useState<ResumeState>({
+    name: '',
+    contact: '',
+    education: [],
+    experience: [],
+    skills: [],
+  });
+
+  const handleSubmit = (data: ResumeState) => {
+    setResume(data);
+  };
+
   return (
-    <div className="app-container">
+    <div className="app">
       <div className="form-container">
         <h1>Resume Builder</h1>
         <p className="instructions">Fill out the form to create your resume</p>
-        <ResumeForm />
-        </div>
+        <ResumeForm onSubmit={handleSubmit} />
+      </div>
       <div className="preview-container">
         <h1>Resume Preview</h1>
-        <ResumePreview />
-        </div>
+        <ResumePreview resume={resume} />
       </div>
+    </div>
   );
 };
 
 export default App;
-
