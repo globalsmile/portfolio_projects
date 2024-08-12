fb6da1a67421bc82fa2118a199a6cf45380ccca2
globalsmile <mohammedbadmus823@gmail.com>
2024-08-12 17:13:51 +0100

    madd: searchMovies component

---
+++ software_projects/front_end_projects/react-movie-search/src/searchMovies.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function SearchMovies() {
+  return (
+    <div>
+      <form className="form">
+        <label htmlFor="query" className="label">
+          Movie Name
+        </label>
+        <input
+          type="text"
+          name="query"
+          placeholder="i.e. Jurassic Park"
+          className="input"
+        />
+        <button type="submit" className="button">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+}
