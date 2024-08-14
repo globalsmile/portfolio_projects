bac5b9ae5787a6a269958d0527fa3c0086527d87
globalsmile <mohammedbadmus823@gmail.com>
2024-08-14 16:03:29 +0100

    create: movies state

---
+++ software_projects/front_end_projects/react-movie-search/src/searchMovies.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "./searchMovies.css";
+export default function SearchMovies() {
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+
+
+  const searchMovies = async (e) => {
+    e.preventDefault();
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=484feddbc117a8605be6c69b8fbf9f9b&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setMovies(data.results);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  return (
+    <div>
+      <form className="form" onSubmit={searchMovies}>
+        <label htmlFor="query" className="label">
+          Movie Name
+        </label>
+        <input
+          type="text"
+          name="query"
+          placeholder="i.e. Jurassic Park"
+          className="input"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit" className="button">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+}
