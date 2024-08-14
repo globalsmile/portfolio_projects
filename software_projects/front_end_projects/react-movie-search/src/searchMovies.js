e027201a350e61da8842381adef38073df7a209b
globalsmile <mohammedbadmus823@gmail.com>
2024-08-14 15:15:27 +0100

    edit: searchMovies.js - add searchmovies function

--- software_projects/front_end_projects/react-movie-search/src/searchMovies.js
+++ software_projects/front_end_projects/react-movie-search/src/searchMovies.js
@@ -1,10 +1,25 @@
 import React from "react";
-import "./searchMovies.css"
-
+import "./searchMovies.css";
 export default function SearchMovies() {
+  const [query, setQuery] = useState('');
+
+  const searchMovies = async (e) => {
+    e.preventDefault();
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <div>
-      <form className="form">
+      <form className="form" onSubmit={searchMovies}>
         <label htmlFor="query" className="label">
           Movie Name
         </label>
@@ -13,6 +28,8 @@ export default function SearchMovies() {
           name="query"
           placeholder="i.e. Jurassic Park"
           className="input"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
         />
         <button type="submit" className="button">
           Search
