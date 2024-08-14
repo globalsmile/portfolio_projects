f45248d9f3a4b1d8835531313f94be3d97d6912f
globalsmile <mohammedbadmus823@gmail.com>
2024-08-14 15:21:01 +0100

    fix: missing import useState

--- software_projects/front_end_projects/react-movie-search/src/searchMovies.js
+++ software_projects/front_end_projects/react-movie-search/src/searchMovies.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./searchMovies.css";
 export default function SearchMovies() {
   const [query, setQuery] = useState('');
