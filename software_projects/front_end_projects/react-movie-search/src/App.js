4148a93b5f8af6f985d73bd08320ca743917d4ad
globalsmile <mohammedbadmus823@gmail.com>
2024-08-14 12:57:45 +0100

    add: searchMovies.css file

--- software_projects/front_end_projects/react-movie-search/src/App.js
+++ software_projects/front_end_projects/react-movie-search/src/App.js
@@ -1,6 +1,3 @@
-/*import logo from './logo.svg';
-import './App.css';
-*/
 import React from "react";
 import SearchMovies from "./searchMovies";
 import "./App.css"
@@ -8,23 +5,6 @@ import "./App.css"
 
 function App() {
   return (
-    /*<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    */
     <html>
       <head>
       </head>
