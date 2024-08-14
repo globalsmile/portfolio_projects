ed584b24b39607e740696c7e13e5c3c313391e2e
globalsmile <mohammedbadmus823@gmail.com>
2024-08-14 17:13:13 +0100

    add: movies component

--- software_projects/front_end_projects/react-movie-search/src/searchMovies.js
+++ software_projects/front_end_projects/react-movie-search/src/searchMovies.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./searchMovies.css";
+import Movies from "./Movies";
 export default function SearchMovies() {
   const [query, setQuery] = useState('');
   const [movies, setMovies] = useState([]);
@@ -40,21 +41,10 @@ export default function SearchMovies() {
 
       <div className="card-list">
         {movies.filter(movie => movie.poster_path).map(movie => (
-          <div className="card" key={movie.id}>
-            <img className="card--image"
-              src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-              alt={movie.title + ' poster'}
-            />
-            <div className="card--content">
-              <h3 className="card--title">{movie.title}</h3>
-              <p><small>RELEASE DATE: {movie.release_date}</small></p>
-              <p><small>RATING: {movie.vote_average}</small></p>
-              <p className="card--desc">{movie.overview}</p>
-            </div>
-
-          </div>
+          <Movies movie={movie} key={movie.id} />
         ))}
       </div>
+      
     </>
   );
 }
