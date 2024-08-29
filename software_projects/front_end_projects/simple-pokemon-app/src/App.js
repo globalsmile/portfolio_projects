26cad712e93d0430e0e77010115385ff9890891f
globalsmile <mohammedbadmus823@gmail.com>
2024-08-29 22:29:23 +0100

    edit: App.js - fix imports

--- software_projects/front_end_projects/simple-pokemon-app/src/App.js
+++ software_projects/front_end_projects/simple-pokemon-app/src/App.js
@@ -3,7 +3,7 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { PokemonsContainer } from './containers/PokemonsContainer';
 
-export default function App() {
+export function App() {
     const client = new ApolloClient({
 	    uri: 'https://graphql-pokemon2.vercel.app'
     });
--- software_projects/front_end_projects/simple-pokemon-app/src/containers/PokemonsContainer.js
+++ software_projects/front_end_projects/simple-pokemon-app/src/containers/PokemonsContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function PokemonsContainer() {
+export function PokemonsContainer() {
     return (
         <p>I am a pokemon container</p>  
     );
