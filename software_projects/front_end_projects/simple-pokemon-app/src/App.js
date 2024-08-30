79c22667edc06c5fc0dc4a92f0063640ee9c6d23
globalsmile <mohammedbadmus823@gmail.com>
2024-08-30 22:58:20 +0100

    add: styles

--- software_projects/front_end_projects/simple-pokemon-app/src/App.js
+++ software_projects/front_end_projects/simple-pokemon-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ApolloClient, ApolloProvider, InMemoryCache }  from '@apollo/client';
 import { PokemonsContainer } from './containers/PokemonsContainer';
-
+import './App.css';
 export default function App() {
     const client = new ApolloClient({
 	    uri: 'https://graphql-pokemon2.vercel.app/',
