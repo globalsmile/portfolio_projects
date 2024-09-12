60192ab39917ed1864156b2c4d008f14eb8fe584
globalsmile <mohammedbadmus823@gmail.com>
2024-09-12 19:45:13 +0100

    add: revised code

--- software_projects/front_end_projects/progress-tracker/src/components/ProgressTracker.js
+++ software_projects/front_end_projects/progress-tracker/src/components/ProgressTracker.js
@@ -7,7 +7,9 @@ const Body = styled.div`
   justify-content: center;
   text-align: center;
   font-family: sans-serif;
+  height: 100vh;
 `;
+
 const Title = styled.h1`
   color: #b19cd9;
   text-align: center;
@@ -15,7 +17,7 @@ const Title = styled.h1`
 
 const Section = styled.div`
   background-color: #ffffff;
-  border: solid 3px grey;
+  border: 3px solid grey;
   margin: 4px;
   width: 300px;
   height: 50px;
@@ -30,37 +32,22 @@ const WeekdayTitle = styled.div`
   display: flex;
   justify-content: center;
   width: 50px;
-  border-right: solid 3px lightgrey;
+  border-right: 3px solid lightgrey;
 `;
 
 const ProgressTracker = () => {
-    return (
-
-        <Body><div><Title>Progress Tracker</Title></div><div>
-            <Section>
-                <WeekdayTitle>M</WeekdayTitle>
-            </Section>
-            <Section>
-                <WeekdayTitle>T</WeekdayTitle>
-            </Section>
-            <Section>
-                <WeekdayTitle>W</WeekdayTitle>
-            </Section>
-            <Section>
-                <WeekdayTitle>T</WeekdayTitle>
-            </Section>
-            <Section>
-                <WeekdayTitle>F</WeekdayTitle>
-            </Section>
-            <Section>
-                <WeekdayTitle>S</WeekdayTitle>
-            </Section>
-            <Section>
-                <WeekdayTitle>S</WeekdayTitle>
-            </Section>
-        </div></Body>
-    );
-
+  return (
+    <Body>
+      <Title>Progress Tracker</Title>
+      <div>
+        {['M', 'T', 'W', 'T', 'F', 'S', 'S'].map((day, index) => (
+          <Section key={index}>
+            <WeekdayTitle>{day}</WeekdayTitle>
+          </Section>
+        ))}
+      </div>
+    </Body>
+  );
 };
 
 export default ProgressTracker;
