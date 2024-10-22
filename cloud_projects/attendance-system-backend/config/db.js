daab42087f5667ee687407ef72a851090ea8cbdc
globalsmile <mohammedbadmus823@gmail.com>
2024-10-22 15:31:19 +0100

    update db parameters

--- cloud_projects/attendance-system-backend/config/db.js
+++ cloud_projects/attendance-system-backend/config/db.js
@@ -4,11 +4,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const dbPassword = process.env.DB_PASSWORD;
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+const dbDatabase = process.env.DB_DATABASE;
 const connection = createConnection({
-  host: 'localhost',
-  user: 'attendance_user',
+  host: dbHost,
+  user: dbUser,
   password: dbPassword,
-  database: 'attendance_system'
+  database: dbDatabase
 });
 
 connection.connect((err) => {
