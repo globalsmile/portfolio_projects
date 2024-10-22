4fa9d0b70e6771e9d2b0a46aba66bc83b1596b73
globalsmile <mohammedbadmus823@gmail.com>
2024-10-22 22:40:49 +0000

    rm directories

--- cloud_projects/attendance-system-backend/routes/attendance.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-import { checkin, checkout } from '../controllers/attendanceController';
-
-router.post('/checkin', checkin);
-router.post('/checkout', checkout);
-
-export default router;
--- cloud_projects/attendance-system-backend/routes/user.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-import { register } from '../controllers/userController';
-
-router.post('/register', register);
-
-export default router;
